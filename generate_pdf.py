5954575e59a13ae221b2311d03e4986e1cc527f2
Bence Janzsó <bence.janzso@gmail.com>
2021-09-09 16:43:04 +0200

    Inserted a new pdf page about portfolio value

--- generate_pdf.py
+++ generate_pdf.py
@@ -9,7 +9,7 @@ def generating_pdf():
     TABLE_COL_NAMES = list(df.columns) 
     TABLE_DATA = []
     for index, rows in df.iterrows():
-        row_list = [pd.Timestamp(rows['Dátum']).strftime("%Y/%m/%d") , f"${str(rows.Ethereum)}", f"${str(rows.BNB)}", f"${str(rows.Cardano)}", f"${str(rows.XRP)}", f"${str(rows.VET)}", f"${str(rows['1inch'])}"]
+        row_list = [pd.Timestamp(rows['Dátum']).strftime("%Y/%m/%d") , f"${str(rows.Ethereum)}", f"${str(rows.BNB)}", f"${str(rows.Cardano)}", f"${str(rows.XRP)}", f"${str(rows['1inch'])}"]
         TABLE_DATA.append(row_list)
 
     # creatinf pdf
@@ -17,13 +17,67 @@ def generating_pdf():
     pdf.add_page()
     pdf.set_font("Times", size=16)
     line_height = pdf.font_size * 2
-    col_width = pdf.epw / 7  # distribute content evenly
+    
 
+    """ Page1: portfolio composition """
+    # page title
+    pdf.set_margins(left=40, top=0, right=40)
+    pdf.set_font('helvetica', 'B', 16)
+    pdf.cell(0, 20, "Portfolió összetétele", ln=1, align="C")
+
+    # table about composition
+    # header
+    col_table_names = ["Valuták", "Mennyiség (db)"]
+    pdf.set_font(style="B", size=14)
+    col_width = pdf.epw / 2
+    for title in col_table_names:
+        pdf.cell(col_width, line_height, title, border=1, align="C")
+
+    pdf.ln(line_height) # break
+    pdf.set_font(style="")  # disabling bold text
+
+    # table body
+    table_content = []
+    df = pd.read_excel('portfolio.xlsx', "Mennyiség")
+    
+    for index, rows in df.iterrows():
+        row_list = [rows[0], str(rows[1])]
+        table_content.append(row_list)
+
+    # create table body
+    for row in table_content:
+        for item in row:
+            pdf.set_font("Times", size=14)
+            pdf.cell(col_width, line_height, item, border=1, align="C")
+        pdf.ln(line_height)
+    
+    # portfolio value
+    # title 
+    pdf.set_font('helvetica', 'B', 16)
+    pdf.cell(0, 25, "Portfolió aktuális értéke", ln=1, align="C")
+
+    # table
+    pdf.set_margins(left=50, top=0, right=50)
+    df = pd.read_excel('portfolio.xlsx', "Érték")
+    current_value = f"${str(df.iloc[-1,1])}" # extract portfolio value from the last row.
+    current_date = pd.Timestamp(df.iloc[-1,0]).strftime("%Y/%m/%d")
+    
+    pdf.set_font("Times", style="B", size=14)
+    col_width = pdf.epw / 2
+    pdf.set_fill_color(37, 196, 49)  # background color
+    pdf.cell(col_width, line_height, current_date, border=1, align="C")
+    pdf.cell(col_width, line_height, current_value, border=1, align="C", fill=True)
+
+
+    """ Page2: list of prices """
     # title for table
+    pdf.add_page()
     pdf.set_font('helvetica', 'B', 16)
     pdf.cell(0, 25, "Árak heti bontásban", ln=1, align="C")
+    pdf.set_margins(left=10, top=0, right=10)
 
     # header in table
+    col_width = pdf.epw / 6  # distribute content evenly
     pdf.set_font(style="B", size=14)  # enabling bold text
     for col_name in TABLE_COL_NAMES:
         if col_name == "Dátum":
@@ -39,6 +93,8 @@ def generating_pdf():
             pdf.set_font("Times", size=14)
             pdf.cell(col_width, line_height, datum, border=1, align="C")
         pdf.ln(line_height)
+    
+    # save pdf
     pdf.output("report.pdf")
 
-#generating_pdf()
+generating_pdf()
--- prices.py
+++ prices.py
@@ -7,7 +7,7 @@ from openpyxl.worksheet.table import Table
 def creating_file():
     # Using Coingecki API
     cg = CoinGeckoAPI()
-    data = cg.get_price(ids='ethereum, binancecoin, cardano, ripple, vechain, 1inch', vs_currencies='usd')
+    data = cg.get_price(ids='ethereum, binancecoin, cardano, ripple, 1inch', vs_currencies='usd')
 
     today = date.today() 
 
@@ -18,7 +18,7 @@ def creating_file():
     ws.title = "Árak"
 
     # add column headings. NB. these must be strings
-    ws.append(["Dátum", "Ethereum", "BNB", "Cardano", "XRP", "VET", "1inch"])
+    ws.append(["Dátum", "Ethereum", "BNB", "Cardano", "XRP", "1inch"])
 
     # appending data from coingecko api
     data = [
@@ -27,7 +27,6 @@ def creating_file():
         data['binancecoin']['usd'], 
         data['cardano']['usd'], 
         data['ripple']['usd'], 
-        data['vechain']['usd'], 
         data['1inch']['usd']
         ],
     ]
@@ -36,13 +35,13 @@ def creating_file():
         ws.append(row)
 
     # Creating table
-    table = Table(displayName="Crypto_prices", ref="A1:G2")
+    table = Table(displayName="Crypto_prices", ref="A1:F2")
 
     ws.add_table(table)
     wb.save("portfolio.xlsx")
 
 
-""" Collecting historical data and append it in a file"""
+""" Collecting historical data (price) and append it in a file"""
 def gather_historic_data(date_request):
     cg = CoinGeckoAPI()
 
@@ -59,9 +58,6 @@ def gather_historic_data(date_request):
     request_xrp = cg.get_coin_history_by_id(id="ripple", date=date_request, localization=False)
     xrp = request_xrp['market_data']['current_price']['usd']
     
-    request_vet = cg.get_coin_history_by_id(id="vechain", date=date_request, localization=False)
-    vet = request_vet['market_data']['current_price']['usd']
-    
     request_1inch = cg.get_coin_history_by_id(id="1inch", date=date_request, localization=False)
     inch = request_1inch['market_data']['current_price']['usd']
 
@@ -77,45 +73,120 @@ def gather_historic_data(date_request):
     year = int(date_request[6:11])
     date_append = date(year, month, day) 
 
-    ws.append([date_append, round(eth, 0), round(bnb, 0), round(ada, 2), round(xrp, 2), round(vet, 4), round(inch, 2)])
+    ws.append([date_append, round(eth, 0), round(bnb, 0), round(ada, 2), round(xrp, 2), round(inch, 2)])
 
     for table in ws.tables.values():
         if table.name == "Árak":
-            table.ref = f"A1:G{ws.max_row}"
+            table.ref = f"A1:F{ws.max_row}"
     
     wb.save("portfolio.xlsx")
 
+def get_historic_value(date_request):
+    cg = CoinGeckoAPI()
+
+    # collecting prices
+    request_eth = cg.get_coin_history_by_id(id="ethereum", date=date_request, localization=False)
+    eth = request_eth['market_data']['current_price']['usd']
+    
+    request_bnb = cg.get_coin_history_by_id(id="binancecoin", date=date_request, localization=False)
+    bnb = request_bnb['market_data']['current_price']['usd']
+    
+    request_ada = cg.get_coin_history_by_id(id="cardano", date=date_request, localization=False)
+    ada = request_ada['market_data']['current_price']['usd']
+    
+    request_xrp = cg.get_coin_history_by_id(id="ripple", date=date_request, localization=False)
+    xrp = request_xrp['market_data']['current_price']['usd']
+    
+    request_1inch = cg.get_coin_history_by_id(id="1inch", date=date_request, localization=False)
+    inch = request_1inch['market_data']['current_price']['usd']
+
+    # Calculate portfolio value
+    wb = load_workbook("portfolio.xlsx")
+    ws_amounts = wb.worksheets[1]
+
+    eth_amount = ws_amounts["B2"].value
+    bnb_amount = ws_amounts["B3"].value
+    ada_amount = ws_amounts["B4"].value
+    xrp_amount = ws_amounts["B5"].value
+    inch_amount = ws_amounts["B6"].value
+
+    portfolio_value = round(
+        (eth_amount * eth) + 
+        (bnb_amount * bnb) + 
+        (ada_amount * ada) + 
+        (xrp_amount * xrp) + 
+        (inch_amount * inch),
+    )
+
+    day = int(date_request[0:2])
+    if date_request[3] == '0':
+        month = int(date_request[4])
+    else:
+        month = int(date_request[3:5])
+    year = int(date_request[6:11])
+    date_append = date(year, month, day) 
+
+    # Append portfolio value in excel
+    ws_value = wb.worksheets[2]
+
+    ws_value.append([date_append, portfolio_value])
+
+    wb.save("portfolio.xlsx")
 
 """ Loading workbook then appending data in new row """
 def append_data():
     # Collect the current prices by CoinGecko API
     cg = CoinGeckoAPI()
-    data = cg.get_price(ids='ethereum, binancecoin, cardano, ripple, vechain, 1inch', vs_currencies='usd')
+    data = cg.get_price(ids='ethereum, binancecoin, cardano, ripple, 1inch', vs_currencies='usd')
 
+    today = date.today()
+
+    # append new prices
     wb = load_workbook("portfolio.xlsx")
-    ws = wb.worksheets[0]
+    ws_prices = wb.worksheets[0]
 
-    today = date.today() 
-
-    ws.append([
+    ws_prices.append([
         today, 
         round(data['ethereum']['usd'], 0), 
         round(data['binancecoin']['usd'], 0), 
         round(data['cardano']['usd'], 2), 
         round(data['ripple']['usd'], 2), 
-        round(data['vechain']['usd'], 4), 
         round(data['1inch']['usd'], 2)
         ])
 
-    for table in ws.tables.values():
+    for table in ws_prices.tables.values():
         if table.name == "Árak":
-            table.ref = f"A1:G{ws.max_row}"
+            table.ref = f"A1:F{ws_prices.max_row}"
         #print(table.name)
         #print(table.ref)
+
+    # Calculate portfolio value
+    ws_amounts = wb.worksheets[1]
+
+    eth_amount = ws_amounts["B2"].value
+    bnb_amount = ws_amounts["B3"].value
+    ada_amount = ws_amounts["B4"].value
+    xrp_amount = ws_amounts["B5"].value
+    inch_amount = ws_amounts["B6"].value
+
+    portfolio_value = round(
+        (eth_amount * data['ethereum']['usd']) + 
+        (bnb_amount * data['binancecoin']['usd']) + 
+        (ada_amount * data['cardano']['usd']) + 
+        (xrp_amount * data['ripple']['usd']) + 
+        (inch_amount * data['1inch']['usd']),
+    )
+
+    # Append portfolio value in excel
+    ws_value = wb.worksheets[2]
+
+    ws_value.append([today, portfolio_value])
 
     wb.save("portfolio.xlsx")
 
 
-#gather_historic_data('22-08-2021')
+#gather_historic_data('05-09-2021')
+get_historic_value('05-09-2021')
 
 #append_data()
+
